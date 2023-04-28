c7cbaa9e16f7693e121712f0e3dc7c456cc09c52
Mohammad-PCSX <100066111+Mohammad-PCSX@users.noreply.github.com>
2023-04-28 09:15:35 +0330

    Add files via upload

---
+++ Client.lua
@@ -0,0 +1,10 @@
+--
+local Blip = AddBlipForCoord(442.3912, -983.433, 30.67834)
+SetBlipSprite(Blip,60)
+SetBlipScale(Blip,1.4)
+SetBlipColour(Blip,38)
+
+local Blip2 = AddBlipForCoord(216.5275, -809.2615, 30.71204)
+SetBlipSprite(Blip2,357)
+SetBlipScale(Blip2,2.0)
+SetBlipColour(Blip2,66)
---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version "cerulean"
+game "gta5"
+
+auther "Mohammad_PCSX"
+description "Add Blip For Server"
+version "1.0.0"
+
+server_script "Server.lua"
+client_script "Client.lua"
